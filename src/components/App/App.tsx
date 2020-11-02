0569db9a849ed13c17cb40b513a849b9076de5e3
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-02 17:24:47 -0300

    Preparando para criar o componente de tabela

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import Header from "../Header";
-import Button from "../../shared/Button";
 import Container from "../../shared/Container";
-import Input from "../../shared/Input";
 
 function App() {
     
-    const [street, setStreet] = useState('')
     
   return (
     <div className="App">
       <Header title={"Stock"} />
       <Container>
-          <Button onClick={() => window.alert('UIIU')}>
-              Click me
-          </Button>   
-          <Input 
-              label={"Street"}
-              placeholder={"E.g: 15th Avenue"}
-              onChange={e => setStreet(e.target.value)}
-          />
       </Container>
     </div>
   );
