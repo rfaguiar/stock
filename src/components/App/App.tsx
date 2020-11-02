eb5fc10567c7f3555894f77fdd329a62905ba6de
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-02 15:33:28 -0300

    Criando o componente Input

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from "../Header";
 import Button from "../../shared/Button";
 import Container from "../../shared/Container";
+import Input from "../../shared/Input";
 
 function App() {
+    
+    const [street, setStreet] = useState('')
+    
   return (
     <div className="App">
       <Header title={"Stock"} />
       <Container>
           <Button onClick={() => window.alert('UIIU')}>
               Click me
-          </Button>          
+          </Button>   
+          <Input 
+              label={"Street"}
+              placeholder={"E.g: 15th Avenue"}
+              onChange={e => setStreet(e.target.value)}
+          />
       </Container>
     </div>
   );
