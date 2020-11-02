3437e2384c5ff52af24fa08381e60288b4f12070
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-02 15:04:43 -0300

    Entendendo tudo sobre Props

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from "../Header";
+import Button from "../Button";
 
 function App() {
   return (
     <div className="App">
-        <Header title={"Stock"} />
-      App
+      <Header title={"Stock"} />
+      <div className={"Container"}>
+          <Button onClick={() => window.alert('UIIU')}>
+              Click me
+          </Button>
+      </div>
     </div>
   );
 }
