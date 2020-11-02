dbc499cfad1e6ee690c767bb233ecdb4700c372a
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-02 12:01:48 -0300

    Componentes funcionais (com hooks)

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import "./TestComponent.css"
 
-const TestComponent = () => (
-    <div className={"TestComponent"}>Test Component!!!</div>
-)
+function TestComponent (props: {name: string}) {
+    
+    const [age, setAge] = useState(20)
+    
+    return <div className={"TestComponent"}>    
+            <p>Olá {props.name} idade {age}, Test Component!!!</p>
+            <button onClick={() => {
+                setAge(age + 1)
+            }}>+</button>
+        </div>
+}
 
 export default TestComponent;
