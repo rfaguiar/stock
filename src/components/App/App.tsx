4c68e1290bf96bcdbd6342082d30e9ebf64eeac7
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-04 19:28:02 -0300

    Entendendo o pattern de serviços

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,12 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Header from "../Header";
 import Container from "../../shared/Container";
 import Table, {TableHeader} from "../../shared/Table";
-import Products, {Product} from "../../shared/Table/Table.mockdata";
+import {Product} from "../../shared/Table/Table.mockdata";
 import ProductForm, {ProductCreator} from "../Products/ProductsForm";
 import Swal from 'sweetalert2';
+import {getAllProducts} from "../../services/Products.service";
 
 const headers: TableHeader[] = [
     { key: 'id', value: '#' },
@@ -15,8 +16,15 @@ const headers: TableHeader[] = [
 ]
 
 function App() {
-    const [products, setProducts] = useState(Products)
+    const [products, setProducts] = useState<Product[]>([])
     const [updatingProduct, setUpdatingProduct] = useState<Product | undefined>(products[0])
+    useEffect(() => {
+        async function fetchData() {
+            const _products = await getAllProducts()
+            setProducts(_products)
+        }
+        fetchData();
+    }, [])
     const handleProductSubmit = (product: ProductCreator) => {
         setProducts([
             ...products,
