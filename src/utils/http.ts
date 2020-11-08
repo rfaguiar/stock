66ee68aea6c2a4d9ece7f95e4b9a546519a05493
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-08 16:19:17 -0300

    create configs for base url use .env and use with nginx.conf in a docker image nginx deploy.

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,14 +1,19 @@
 import axios from "axios";
 import {store} from "../redux";
 
+const baseURL = process.env.REACT_APP_API_BASE_URL
+console.log('baseURL', baseURL)
 const http = axios.create({
-    baseURL: 'https://rfaguiar-api-stock.herokuapp.com'
+    baseURL,
 });
 
 http.interceptors.request.use(config => {
     const token = store.getState().authentication.profile?.token
     if (token) {
         config.headers.Authorization = `Bearer ${token}`        
+    }
+    if (!baseURL) {
+        config.url = `/api${config.url}`
     }
     return config
 })
